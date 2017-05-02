c8937ffe3b7973c56dab29408869cbbb8235baa9
Seospan <sophie.jobez@gmail.com>
2017-05-02 18:14:41 +0200

    Chose method 5 and cleaned for multiple instanciation of dataService
    http://stackoverflow.com/questions/43734574/angular-4-how-to-pass-an-argument-to-a-service-provided-in-a-module-file

--- src/app/home/dashboard/multi-attribution/multi-attribution.module.ts
+++ src/app/home/dashboard/multi-attribution/multi-attribution.module.ts
@@ -12,23 +12,11 @@ import { OverviewComponent } from './overview/overview.component';
 import { DataRequestService } from '../services/data-request.service';
 import { DataFiltersService } from '../services/data-filters.service';
 import { DataService } from '../services/data.service';
+import { dataServiceFactory } from '../services/data.service';
 
 import viewConfig from './view.config.json';
-
-//Methode 1 : injection without ''
-//export const VIEW_CONFIG = new InjectionToken<any>('test');
-
-//Methode 2 : injection with '' : nothing
-
-//Methode 3 : factory
-/*export let VIEW_CONFIG = new InjectionToken<any>('');
-let dataServiceFactory = (configObject):DataService => { return new DataService(configObject) }*/
-
-//Methode 4 : custom provider with direct value call to "new". Usable also with factory
-//4B
-//let dataServiceFactory = () => { return new DataService(viewConfig) }
-//4C
-//let dataServiceFactory = (configObject) => { return new DataService(configObject) }
+//Methode 5 :
+export const VIEW_CONFIG = new InjectionToken<any>('test');
 
 @NgModule({
     imports: [
@@ -43,25 +31,9 @@ let dataServiceFactory = (configObject):DataService => { return new DataService(
     providers: [
         DataRequestService,
         DataFiltersService,
-//Methode 1 : injection without ''
-        /*{ provide: VIEW_CONFIG, useValue: viewConfig },
-        DataService,*/
-//Meethode 2 : injection with ' '
-        /*{ provide: 'VIEW_CONFIG', useValue: viewConfig },
-        DataService,*/
-//Methode 3 : factory
-        /*{ provide: VIEW_CONFIG, useValue: viewConfig },
-        {
-            provide : DataService,
-            useFactory : dataServiceFactory,
-            deps : [VIEW_CONFIG]
-        },*/
-//Methode 4 : custom provider with direct value
-        { provide: DataService, useValue : new DataService(viewConfig) },
-//4B
-        //{ provide: DataService, useValue : dataServiceFactory() },
-//4C
-        //{ provide: DataService, useValue : dataServiceFactory(viewConfig) },
+//Methode 5 : factory from service class
+        { provide: VIEW_CONFIG, useValue: viewConfig },
+        { provide : DataService, useFactory: dataServiceFactory, deps : [VIEW_CONFIG], multi: true }
     ],
 })
 
