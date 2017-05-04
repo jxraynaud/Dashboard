f9e4a6fb9c297dc07df863c21c6a8c1dcf94dc06
Seospan <sophie.jobez@gmail.com>
2017-05-04 18:22:46 +0200

    Updated debug functions

--- src/app/utils.ts
+++ src/app/utils.ts
@@ -1,2 +1,7 @@
-export function debugLog(DEBUG, str){ DEBUG && console.log(str) }
-export function debugLogGroup(DEBUG, strArray){ if(DEBUG){ for(let e in strArray){ e == '0' ? console.groupCollapsed(strArray[e]):console.log(strArray[e]) ;} console.groupEnd() } }
+const FORCE_ALL_DEBUG : boolean = false;
+
+export function debugLog(DEBUG, str){ (DEBUG || FORCE_ALL_DEBUG) && console.log(str) }
+
+export function debugWarn(DEBUG, str){ (DEBUG || FORCE_ALL_DEBUG) && console.warn(str) }
+
+export function debugLogGroup(DEBUG, strArray){ if(DEBUG || FORCE_ALL_DEBUG){ for(let e in strArray){ e == '0' ? console.groupCollapsed(strArray[e]):console.log(strArray[e]) ;} console.groupEnd() } }
