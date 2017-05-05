b1aa52e2c933e381726fdecca088a8fc6a03911c
Seospan <sophie.jobez@gmail.com>
2017-05-05 18:17:50 +0200

    Calculate and add name columns to dataviz-datatable

--- src/app/home/dashboard/dashboard-assets/dataviz-datatable/dataviz-datatable.component.ts
+++ src/app/home/dashboard/dashboard-assets/dataviz-datatable/dataviz-datatable.component.ts
@@ -18,7 +18,34 @@ export class DatavizDatatableComponent implements OnInit {
     DEBUG: boolean = false;
 
     @Input() filteredData : Array<{}>;
-    @Input() dimensionColumns : Array<{}>;
+    private aggregatedFilteredData : Array<{}>;
+
+    @Input() dimensionColumnsWithId : ITdDataTableColumn[] = [];
+    @Input() dimensionColumnsWithoutId : ITdDataTableColumn[] = [];
+    //measuresColumns : ITdDataTableColumn[] = [];
+
+    //Columns that represent an id. To be used by the "show ids" column
+    //private withoutIdColumns : ITdDataTableColumn[] = [];
+    //private withIdColumns : ITdDataTableColumn[] = [];
+
+    //public columns : ITdDataTableColumn[] = [];
+
+    //public filteredByDataTableData: any[];
+    //public filteredTotal: number;
+    //
+    //searchTerm: string = '';
+    //fromRow: number = 1;
+    //currentPage: number = 1;
+    //pageSize: number = 50;
+    //sortBy: string = 'conversions';
+    //sortOrder: TdDataTableSortingOrder = TdDataTableSortingOrder.Descending;
+    //
+    //@Input() activeGroupByFields : string[]=[];
+    //@Input() availableGroupByFields : string[];
+    //@Input() defaultGroupByFields : string[];
+    //@ViewChild('pagingBar') pagingBar
+
+    //displayIdsInDatatable : boolean = false;
 
     constructor(
         private _dataTableService: TdDataTableService) {
--- src/app/home/dashboard/multi-attribution/overview/overview.component.ts
+++ src/app/home/dashboard/multi-attribution/overview/overview.component.ts
@@ -15,8 +15,9 @@ import {debugLog, debugWarn, debugLogGroup} from '../../../../utils';
 export class OverviewComponent implements OnInit {
     DEBUG : boolean = true;
 
-    activeDimensions : string[] = ['advertiser_id','partner_id','kpi_id','bbb'];
-    activeDimensionColumns : Array<{}>;
+    activeDimensions : string[] = ['advertiser_id','partner_id','kpi_id','metacampaign_id','bbb'];
+    activeDimensionWithIdColumns : Array<{}>;
+    activeDimensionsWithoutIdColumns : Array<{}>;
 
     activeMetrics : string[] = ['conversion_date','conversions Default 7.5/7.5','conversions Sizmek 30/15'];
     activeMetricColumns : Array<{}>;
@@ -55,51 +56,61 @@ export class OverviewComponent implements OnInit {
                     "List of active columns : ",
                     this.activeDimensions
                 ]);
-                this.activeDimensionColumns = this.generateDimensionColumnsList(configData['available_dimensions']);
+                this.activeDimensionWithIdColumns = this.generateDimensionColumnsListsObject(configData['available_dimensions']).withIdColumns;
+                this.activeDimensionsWithoutIdColumns = this.generateDimensionColumnsListsObject(configData['available_dimensions']).withoutIdColumns;
             },
             error : (err) => console.error(err),
         });
-        //console.log(this.configService);
-
-        /*this.availableColumnsDetailsBehaviorSubjectSubscription = this.dataService.availableColumnsDetailsBehaviorSubject.subscribe({
-            next : (availableColumns) => {
-                debugLogGroup(this.DEBUG,["Overview Component : this.dataService.availableColumnsDetailsBehaviorSubject subscription triggered with value [availableColumns] :",
-                    availableColumns,
-                    "Calculating active dimensions details and building columns info for DataTable"
-                ]);
-                let activeDimensionsColumns = [];
-                for( let colIndex in availableColumns ){
-                    if( this.activeDimensions.indexOf(colIndex) != -1 ){
-                        console.log(colIndex);
-                        console.log(availableColumns[colIndex]);
-                    }
-                }
-            },
-            error : (err) => console.error(err),
-        });*/
     }
+    /**
+     * Generates 2 things : list of columns with id columns and list of columns withour id columns
+     * @method generateDimensionColumnsListsObject
+     * @param  {[type]}                            availableDimensions [description]
+     * @return {{withIdColumns:[],withoutIdColumns:[]}}     Object containing both with id and withoutIds columns
+     */
+    private generateDimensionColumnsListsObject(availableDimensions){
+        //Creating empty arrays for column list
+        let activeWithIdDimensionColumnsTemp = [];
+        let activeWithoutIdDimensionColumnsTemp = [];
 
-    private generateDimensionColumnsList(availableDimensions){
-        let activeDimensionColumnsTemp = [];
         this.activeDimensions.map((dimensionName)=>{
             let singleActiveDimension = availableDimensions.filter((e)=>{ return e.data_id_column_name == dimensionName});
+
             if(singleActiveDimension.length==0){
                 //Error catch : if an unavailable dimension was listed, skip it.
                 console.error('"'+dimensionName+'" is not an available dimension. Dimension ignored. See list of available dimensions : ',[availableDimensions[0]]);
             }else{
-                let singleActiveDimensionColumn = {
-                    name : singleActiveDimension[0].data_id_column_name,
-                    label : singleActiveDimension[0].label,
-                    numeric : singleActiveDimension[0].is_numeric
+                //Filter first and only element returned by filter
+                singleActiveDimension = singleActiveDimension[0];
+                //Create id column
+                let singleActiveDimensionIdColumn = {
+                    name : singleActiveDimension.data_id_column_name,
+                    label : singleActiveDimension.id_label,
+                    numeric : true
                 };
-                activeDimensionColumnsTemp.push(singleActiveDimensionColumn);
+                //Puushing element in temporary column list
+                activeWithIdDimensionColumnsTemp.push(singleActiveDimensionIdColumn);
+
+                //Create name column if indicated in config file
+                if(singleActiveDimension.data_name_column_name){
+                    let singleActiveDimensionNameColumn = {
+                        name : singleActiveDimension.data_name_column_name,
+                        label : singleActiveDimension.label,
+                        numeric : singleActiveDimension.is_numeric
+                    };
+                    activeWithIdDimensionColumnsTemp.push(singleActiveDimensionNameColumn);
+                    activeWithoutIdDimensionColumnsTemp.push(singleActiveDimensionNameColumn);
+                }else{
+                    debugWarn(this.DEBUG,"OverviewComponent : "+singleActiveDimension.data_id_column_name+" has no indicated human readable column in config file. Skipping name column.")
+                }
             }
         });
         debugLogGroup(this.DEBUG,[
-            "Overview component : Calculated list of columns depending on list of active columns and list of available columns :",
-            activeDimensionColumnsTemp
+            "Overview component : Calculated list of columns depending on list of active columns and list of available columns, returning both with and without id columns [ withIdColumns,withoutIdColumns ]:",
+            activeWithIdDimensionColumnsTemp,
+            activeWithoutIdDimensionColumnsTemp
         ]);
-        return activeDimensionColumnsTemp;
+        return {withIdColumns:activeWithIdDimensionColumnsTemp, withoutIdColumns:activeWithoutIdDimensionColumnsTemp};
     }
 
 
