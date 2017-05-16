ef86984c5a1f55d962095b4ece56b2d127c06299
Raynaud <jxraynaud@gmail.com>
2017-05-16 17:33:16 +0200

    Added input to the by kpi view

--- src/app/home/dashboard/multi-attribution/bykpiview/bykpiview.component.ts
+++ src/app/home/dashboard/multi-attribution/bykpiview/bykpiview.component.ts
@@ -1,4 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+
+import {
+    TdDataTableService,
+    TdDataTableSortingOrder,
+    ITdDataTableSortChangeEvent,
+    ITdDataTableColumn,
+    IPageChangeEvent } from '@covalent/core';
 
 @Component({
   selector: 'app-bykpiview',
@@ -6,10 +13,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./bykpiview.component.css']
 })
 export class BykpiviewComponent implements OnInit {
+    DEBUG : boolean = true;
 
-  constructor() { }
+    //Unnamed yet filtered data (to be named by dataviz)
+    @Input() filteredData : Array<{}>;
+    @Input() activeDimensionsWithIdColumns : ITdDataTableColumn[];
+    @Input() activeDimensionsWithoutIdColumns : ITdDataTableColumn[];
+    @Input() activeStaticMetricsColumns : ITdDataTableColumn[];
+    @Input() additiveMetricsList : Array<string>;
+    @Input() filtersDimensionMapping;
+    @Input() config;
+    //Setting to pipe to the views to define if the attribution model parameter of the api is simple (number) or multiple (array)
+    @Input() isAttributionModelMultiple : boolean;
 
-  ngOnInit() {
-  }
+    constructor() { }
+
+    ngOnInit() {
+    }
 
 }
