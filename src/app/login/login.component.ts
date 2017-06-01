511f41a819f460659177bbf7185c009fcce8f670
Seospan <sophie.jobez@gmail.com>
2017-06-01 17:39:31 +0200

    STABLE - Login rturnurl bug on query parameters not decoded properly

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -16,8 +16,9 @@ import { AlertService } from '../services/alert.service';
 })
 export class LoginComponent implements OnInit {
 
-    model: any = {};
+    model : any = {};
     returnUrl: string;
+    returnUrlParams : Object;
 
     constructor(
         private route:ActivatedRoute,
@@ -31,18 +32,30 @@ export class LoginComponent implements OnInit {
         //this.authenticationService.logout();
 
         // get return url from route parameters or default to '/'
-        this.returnUrl = this.route.snapshot.queryParams['returnUrl'] || '/';
+        this.returnUrl = this.route.snapshot.queryParams['returnUrl'].split("?")[0] || '/';
+        this.returnUrlParams = this.getQueryParamsObject(this.route.snapshot.queryParams['returnUrl']);
     }
 
     login(): void {
         this.authenticationService.login(this.model.username, this.model.password)
             .subscribe(
                 data => {
-                    this.router.navigate([this.returnUrl]);
+                    this.router.navigate([this.returnUrl], { queryParams: this.returnUrlParams } );
                 },
                 error => {
                     this.alertService.error(error._body);
                 }
             );
+    }
+
+    getQueryParamsObject(url){
+        let paramsArray = url.split("?")[1].split("&");
+        let paramsObject = {};
+        paramsArray.map(e=>{
+            let key = e.split("=")[0];
+            let value = e.split("=")[1];
+            paramsObject[key]=value;
+        })
+        return paramsObject;
     }
 }
